fa557299d047ca262335866b9ad5e9f7c885b3d3
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2023-01-07 19:01:36 +0800

    add the port and change the file path name more mistakes falled in update

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Genious car is running')
+})
+app.listen(port, () => {
+    console.log(`Genious car was running on ${port}`)
+})
