f12f07652b84751e03c24cd053253221151fb4e9
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2023-01-08 09:30:16 +0800

    delete function addded

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function run() {
         const serviceCollection = client.db('watchResale').collection('reSale');
         const accsoriesCollection = client.db('watchResale').collection('accSories');
         const orderCollection = client.db('watchResale').collection('orders');
-        
+
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
@@ -49,13 +49,35 @@ async function run() {
             res.send(service)
         })
 
+
         // orders api
+
+        app.get('/orders', async (req, res) => {
+            let query = {};
+            if (req.query.email) {
+                query = {
+                    email: req.query.email
+                }
+            }
+            const cursor = orderCollection.find(query)
+            const orders = await cursor.toArray();
+            res.send(orders);
+        })
+
         app.post('/orders', async (req, res) => {
             const order = req.body;
             const result = await orderCollection.insertOne(order)
             res.send(result)
         })
 
+        // delete the products will called there
+
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await orderCollection.deleteOne(query)
+            res.send(result);
+        })
     }
     finally {
 
